799719860bad810b9ce6f33624d8f658fb0feb1d
Antame <Anne.Martine.META.MUSASA@cf2000.onmicrosoft.com>
2021-11-05 21:12:06 +0100

    ajout entete menu

--- PUBLIC_PAGES/menu.php
+++ PUBLIC_PAGES/menu.php
@@ -1,5 +1,6 @@
 <nav class="menu">
     <ul>
+        <li class="home"><a href="#">Menu</a></li>
         <li><a href="./?pg=Accueil">Acceuil</a></li>
         <li><a href="./?pg=CV">CV</a></li>
         <li><a href="./?pg=Gallerie">Gallerie</a></li>
